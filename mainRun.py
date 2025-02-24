ba29c94b5b4c2b708b5bf007a1f99de819b4bbf0
HarshavardhanM13 <151740329+HarshavardhanM13@users.noreply.github.com>
2025-02-24 21:58:21 +0530

    Update mainRun.py

--- mainRun.py
+++ mainRun.py
@@ -2,24 +2,24 @@ import joblib
 import pandas as pd
 import plotgraph
 
-# Load the trained model and scaler
+
 model = joblib.load('DehydrationModel.pkl')
 scaler = joblib.load('scaler.pkl')
 
-# Function to predict dehydration
+
 def predict_dehydration(input_data):
-    # Ensure input_data is a DataFrame with the correct columns
+  
     input_data_scaled = scaler.transform(input_data)
     prediction = model.predict(input_data_scaled)
     return 'Yes' if prediction[0] == 1 else 'No'
 
-# User input
+
 water_intake = float(input("Enter water intake (liters): "))
 physical_activity = float(input("Enter physical activity (hours): "))
 ambient_temperature = float(input("Enter ambient temperature (°C): "))
 sweat_rate = float(input("Enter sweat rate (liters/hour): "))
 
-# Create the input DataFrame with matching column names
+
 input_data = pd.DataFrame({
     'Water_Intake (liters)': [water_intake],
     'Physical_Activity (hours)': [physical_activity],
@@ -27,7 +27,7 @@ input_data = pd.DataFrame({
     'Sweat_Rate (liters/hour)': [sweat_rate]
 })
 
-# Predict dehydration
+
 result = predict_dehydration(input_data)
 print(f"Predicted Dehydration Symptoms: {result}")
 
